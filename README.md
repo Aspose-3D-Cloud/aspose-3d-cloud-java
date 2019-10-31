82cfdcac3a49d28700130b31988346d867f7dab0
xinyagithub <xinya.zhu@aspose.com>
2019-10-31 14:50:12 +0800

    Init Version

---
+++ README.md
@@ -0,0 +1,55 @@
+[Aspose.3D Cloud](https://products.aspose.cloud/3D) contains Aspose.3D Cloud SDK for Java source code. This SDK allows you to work with Aspose.3D Cloud REST APIs in your java applications quickly and easily.
+
+To use this SDK, you will need App SID and App Key which can be looked up at [Aspose Cloud Dashboard](https://dashboard.aspose.cloud/#/apps) (free registration in Aspose Cloud is required for this).
+
+
+# Key Features
+
+- Conversion between various document-related formats (10+ formats supported)
+
+- Modeling and Data Processing,including parametric modeling and delete object from scene 
+
+- Utilities,including extract 3D contens from PDF file and triangulate meshes 
+
+
+
+See [API Reference](https://apireference.aspose.cloud/3d/) for full API specification.
+
+
+
+# File,folder and storage API support
+
+
+It gives you an ability to:
+
+- Upload, download, copy, move and delete files, including versions handling (if you are using Cloud storage that supports this feature - true by default)
+- Create, copy, move and delete folders
+- Copy and move files and folders accross separate storages in scope of a single operation
+- Check if certain file, folder or storage exists
+
+# Licensing
+
+All Aspose.3D Cloud SDKs are licensed under [MIT License](https://github.com/aspose-3d-cloud/aspose-3d-cloud-dotnet/blob/master/LICENSE).
+
+# How to use the SDK?
+
+# Tests
+
+[Tests](https://github.com/aspose-3D-cloud/aspose-3D-cloud-java/tree/master/Aspose.3D.Cloud.SDK.Test) contain various examples of using the SDK.
+
+
+
+# Contact Us
+
+Your feedback is very important to us. Please feel free to contact via
+
+- [**Free Support Forum**](https://forum.aspose.cloud/c/3d)
+- [**Paid Support Helpdesk**](https://helpdesk.aspose.cloud/)
+
+# Resources
+
+- [**Web API reference**](https://apireference.aspose.cloud/3d/)
+- [**Website**](https://www.aspose.cloud)
+- [**Product Home**](https://products.aspose.cloud/3d)
+- [**Documentation**](https://docs.aspose.cloud/display/3dcloud/Home)
+- [**Blog**](https://blog.aspose.cloud/category/3d/)
---
+++ src/test/java/com/aspose/cloud/threed/api/ThreeDBaseTest.java
@@ -0,0 +1,67 @@
+package com.aspose.cloud.threed.api;
+
+import com.aspose.cloud.threed.client.ApiClient;
+import com.aspose.cloud.threed.client.ApiException;
+import com.aspose.cloud.threed.model.AccessTokenResponse;
+
+public class ThreeDBaseTest {
+ 
+    protected ThreeDCloudApi threeDCloudApi;
+   
+	private static String accesstoken;
+	private static String grantType = "client_credentials";
+	private static String clientId =  "****yourAppid****";
+	private static String clientSecret =  "****yourAppKey****";
+	private static String sourceFolder ="E:\\Cloud\\src\\java\\testData\\Aspose.pdf";
+	public static String GetSourceFolder() {
+		return sourceFolder;
+	}
+	public static String GetGrantType() {
+		return grantType;
+	}
+
+	public static String GetClientId() {
+		return clientId;
+	}
+
+	public static String GetClientSecret() {
+		return clientSecret;
+	}
+
+ 
+    public ThreeDBaseTest()
+    {
+    	threeDCloudApi = new ThreeDCloudApi(); 
+    	threeDCloudApi.setApiClient(this.Ready());
+    }        
+ 
+	public static String GetAccessToken() {
+		try {
+			if (accesstoken == null || accesstoken == "") {
+				System.out.println("--------------------------");
+				ApiClient apiClient = new ApiClient();
+				apiClient.setBasePath("https://api.aspose.cloud");
+				ThreeDCloudApi oauth2 = new ThreeDCloudApi();
+				oauth2.setApiClient(apiClient);
+				AccessTokenResponse accessTokenResponse = oauth2.oAuthPost(grantType, clientId, clientSecret);
+				accesstoken = accessTokenResponse.getAccessToken();
+				System.out.println(accesstoken);
+				System.out.println("--------------------------");
+			}
+
+		} catch (ApiException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return accesstoken;
+	}
+
+	public static ApiClient Ready() {
+		ThreeDCloudApi apiTask = new ThreeDCloudApi();
+		ApiClient apiClient = new ApiClient();
+		apiClient.setBasePath("https://api.aspose.cloud/v3.0");
+		apiClient.addDefaultHeader("Authorization", "Bearer " + GetAccessToken());
+		apiTask.setApiClient(apiClient);
+		return apiClient;
+	}
+}
